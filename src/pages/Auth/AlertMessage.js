909dd56501b842f7d27eca88dae4129142605ccc
Hieu Minh Vu <hieuminhvuu@AlienBookpro.local>
2022-11-09 15:34:05 +0300

    login-register page design

--- src/pages/Auth/AlertMessage.js
+++ src/pages/Auth/AlertMessage.js
@@ -1,9 +1,8 @@
 import React from "react";
-import Alert from "react-bootstrap/Alert";
 
 const AlertMessage = ({ info }) => {
     return info === null ? null : (
-        <Alert variant={info.type}>{info.message}</Alert>
+        <p style={{ color: "red" }}>{info.message}</p>
     );
 };
 
