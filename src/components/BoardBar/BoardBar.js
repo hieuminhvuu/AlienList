285432984bceed18786cd06a223342407e0f533d
Do Manh <domanhitmo@gmail.com>
2023-01-21 18:38:25 +0700

    Front-end

--- src/components/BoardBar/BoardBar.js
+++ src/components/BoardBar/BoardBar.js
@@ -3,6 +3,34 @@ import React from "react";
 import "./BoardBar.scss";
 import { Container as BootstrapContainer, Row, Col } from "react-bootstrap";
 
+const date: Date = new Date();
+const year: number = date.getFullYear();
+const month: number = date.getMonth();
+const day: number = date.getDate();
+
+const monthName: string[] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+const todayDate = `${year}, ${monthName[month].slice(0, 3)} ${day
+    .toString()
+    .padStart(2, "0")}`;
+
+const dateTimeFormat = `${year}-${month.toString().padStart(2, "0")}-${day
+    .toString()
+    .padStart(2, "0")}}`;
+
 function BoardBar() {
     return (
         <nav className="navbar-board">
@@ -12,11 +40,8 @@ function BoardBar() {
                         <div className="board-info">
                             <div className="item board-logo-icon">
                                 <i className="fa fa-coffee" />
-                                &nbsp;&nbsp;<strong>WorkSpace</strong>
+                                &nbsp;&nbsp;<time dateTime={dateTimeFormat}>{todayDate}</time>
                             </div>
-                            <div className="divider"></div>
-                            <div className="item board-type">Private</div>
-                            <div className="divider"></div>
                         </div>
                     </Col>
                     <Col sm={2} xs={12} className="col-no-padding">
